eabe1833a2af77492811d03e201d6e5360f724aa
poojakhandale <poojakhandale2000@gmail.com>
2023-03-09 00:19:10 +0530

    Contact logic

--- Contacts.cs
+++ Contacts.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace SetsDay23
 {
-    internal class Contacts
+    public class Contacts
     {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string country { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+
+
+        public Contacts(string firstName, string lastName, string country, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.country = country;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
     }
 }
+
