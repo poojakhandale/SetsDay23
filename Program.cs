bfa2ca32e3d1d5681344701e777c27d409fd5d4f
poojakhandale <poojakhandale2000@gmail.com>
2023-03-09 00:20:05 +0530

    search for contacts by country/city

--- Program.cs
+++ Program.cs
@@ -10,11 +10,12 @@ namespace Day23Sets
             AddressBook myAddressBook = new AddressBook();
             Contacts newContact = new Contacts("Pooja", "khandale", "INDIA", "8369319177", "pooja.p@example.com");
             Contacts newContact2 = new Contacts("Rani", "Dube", "USA", "888222444", "rani.dube@jr.com");
-            Contacts newContact3 = new Contacts("Rani", "Dube", "USA", "888222444", "rani.dube@jr.com");
+            Contacts newContact3 = new Contacts("preeti", "sharma", "USA", "888222444", "rani.dube@jr.com");
 
             myAddressBook.AddContact(newContact);
             myAddressBook.AddContact(newContact2);
             myAddressBook.AddContact(newContact3);
+            myAddressBook.SearchByCountry("USA");
             //myAddressBook.PrintContacts();
         }
     }
