621312e9d79c71dcba9fbb53b9ebad9b67747184
poojakhandale <poojakhandale2000@gmail.com>
2023-03-09 00:40:07 +0530

    Added method to get count by country

--- AddressBook.cs
+++ AddressBook.cs
@@ -5,57 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 
+
+
 namespace Day23Sets
-{
-    public class AddressBook
     {
-        public List<Contacts> ContactInfo { get; set; }
-        public Dictionary<string, List<Contacts>> ContactsByCountry { get; set; }
-
-        public AddressBook()
+        public class AddressBook
         {
-            ContactInfo = new List<Contacts>();
-            ContactsByCountry = new Dictionary<string, List<Contacts>>();
-        }
+            public List<Contacts> ContactInfo { get; set; }
+            public Dictionary<string, List<Contacts>> ContactsByCountry { get; set; }
 
-        public void AddContact(Contacts contact)
-        {
-            if (!ContactInfo.Contains(contact))
+            public AddressBook()
             {
-                ContactInfo.Add(contact);
+                ContactInfo = new List<Contacts>();
+                ContactsByCountry = new Dictionary<string, List<Contacts>>();
+            }
 
-                if (!ContactsByCountry.ContainsKey(contact.country))
+            public void AddContact(Contacts contact)
+            {
+                if (!ContactInfo.Contains(contact))
                 {
-                    ContactsByCountry[contact.country] = new List<Contacts>();
+                    ContactInfo.Add(contact);
+
+                    if (!ContactsByCountry.ContainsKey(contact.country))
+                    {
+                        ContactsByCountry[contact.country] = new List<Contacts>();
+                    }
+                    ContactsByCountry[contact.country].Add(contact);
                 }
-                ContactsByCountry[contact.country].Add(contact);
             }
-        }
 
-        public void PrintContacts()
-        {
-            foreach (var contact in ContactInfo)
+            public void PrintContacts()
             {
-                Console.WriteLine("Name: {0} {1}\nCountry: {2}\nPhone: {3}\nEmail: {4}\n",
-                    contact.firstName, contact.lastName, contact.country, contact.phoneNumber, contact.email);
+                foreach (var contact in ContactInfo)
+                {
+                    Console.WriteLine("Name: {0} {1}\nCountry: {2}\nPhone: {3}\nEmail: {4}\n",
+                        contact.firstName, contact.lastName, contact.country, contact.phoneNumber, contact.email);
+                }
             }
-        }
 
-        public List<Contacts> GetContactsByCountry(string country)
-        {
-            return ContactsByCountry.ContainsKey(country) ? ContactsByCountry[country] : new List<Contacts>();
-        }
-
-        public void PrintContactCountByCountry()
-        {
-            Console.WriteLine("Contact Count By Country:");
-
-            foreach (var kvp in ContactsByCountry)
+            public List<Contacts> GetContactsByCountry(string country)
             {
-                Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value.Count);
+                return ContactsByCountry.ContainsKey(country) ? ContactsByCountry[country] : new List<Contacts>();
+            }
+
+            public void PrintContactCountByCountry()
+            {
+                Console.WriteLine("Contact Count By Country:");
+
+                foreach (var kvp in ContactsByCountry)
+                {
+                    Console.WriteLine("{0}: {1}", kvp.Key, kvp.Value.Count);
+                }
             }
         }
     }
-}
 
 
