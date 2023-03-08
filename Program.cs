665625204ee337818fce6b0a4fa5239a75345243
poojakhandale <poojakhandale2000@gmail.com>
2023-03-08 23:10:40 +0530

    trying to add duplicate value

--- Program.cs
+++ Program.cs
@@ -1,12 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-class program
+using SetsDay23;
+
+namespace Day23Sets
 {
-
-
-    public static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Welcome to the master branch for day 23 Assignments");
+        public static void Main(string[] args)
+        {
+            AddressBook myAddressBook = new AddressBook();
+            Contacts newContact = new Contacts("Pooja", "khandale", "INDIA", "8369319177", "pooja.p@example.com");
+            Contacts newContact2 = new Contacts("Rani", "Dube", "USA", "888222444", "rani.dube@jr.com");
+            Contacts newContact3 = new Contacts("Rani", "Dube", "USA", "888222444", "rani.dube@jr.com");
+
+            myAddressBook.AddContact(newContact);
+            myAddressBook.AddContact(newContact2);
+            myAddressBook.AddContact(newContact3);
+            //myAddressBook.PrintContacts();
+        }
     }
-
 }
-
