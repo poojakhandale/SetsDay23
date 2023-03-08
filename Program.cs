891b4f7b8eac80aaa23b8184ca49ec0601c01134
poojakhandale <poojakhandale2000@gmail.com>
2023-03-08 22:54:07 +0530

    Greeting massage

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+
+
+public static void Main(string[] args)
+{
+    Console.WriteLine("Welcome to the master branch for day 23 Assignments");
+}
+    }
+
